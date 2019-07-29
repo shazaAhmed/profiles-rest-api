9532917f57054b6967a53c48e3a9cb29b690a98f
Ahmedsalaheld <shazaahmedd@icloud.com>
2019-07-29 12:10:07 +0200

    added searching to user profile

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -2,6 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response 
 from rest_framework import status
 from rest_framework import viewsets
+from rest_framework import filters
 from rest_framework.authentication import TokenAuthentication
 
 
@@ -109,4 +110,6 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     queryset = models.UserProfile.objects.all()
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email',)
 
