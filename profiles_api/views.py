80764f0b80a24a3c2f588ecb7588e78d8fcb6b46
Ahmedsalaheld <shazaahmedd@icloud.com>
2019-07-29 12:48:12 +0200

    added login API

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -4,6 +4,8 @@ from rest_framework import status
 from rest_framework import viewsets
 from rest_framework import filters
 from rest_framework.authentication import TokenAuthentication
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
 
 from profiles_api import serializers
@@ -100,7 +102,6 @@ class HelloViewSet(viewsets.ViewSet):
 
         def destroy(self, request, pk=None):
             """Handle removing an object"""
-
             return Response({'http_method': 'DELETE'})
 
 
@@ -112,4 +113,8 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     permission_classes = (permissions.UpdateOwnProfile,)
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name', 'email',)
+
+class UserLoginApiView(ObtainAuthToken):
+    """Handle creating user authentication tokens"""
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
 
